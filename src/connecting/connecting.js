e7230d323e6ffee707e34aeb0ebc8e3e586ac570
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-07 21:06:30 +0530

    Update connecting.js

--- src/connecting/connecting.js
+++ src/connecting/connecting.js
@@ -22,9 +22,9 @@ const handleApiResponse = (apiCall) => {
         apiCall
             .then((res) => resolve(res.data))
             .catch((error) => {
-                const errorMessage = error.response?.data?.match(/Error: ([\s\S]*?)<br>/);
-                if (errorMessage) reject(errorMessage[1]);
-                else reject("unknown error");
+                //const errorMessage = error.response?.data?.match(/Error: ([\s\S]*?)<br>/);
+                //if (errorMessage) reject(errorMessage[1]);
+                else reject(error);
             });
     });
 };
